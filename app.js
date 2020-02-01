2525b8c70a7d2c0067c4a43270d3c813b4743498
Dave Horton <daveh@beachdognet.com>
2020-02-01 16:16:00 -0500

    added initial support for REST-initiated outdials

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const {
   invokeWebCallback
 } = require('./lib/middleware')(srf, logger);
 
+// HTTP 
+const PORT = process.env.HTTP_PORT || 3000;
+const express = require('express');
+const app = express();
+app.locals.logger = logger;
+const httpRoutes = require('./lib/http-routes');
+
 const InboundCallSession = require('./lib/session/inbound-call-session');
 
 // disable logging in test mode
@@ -45,5 +52,18 @@ srf.invite((req, res) => {
   const session = new InboundCallSession(req, res);
   session.exec();
 });
+
+
+// HTTP
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', httpRoutes);
+app.use((err, req, res, next) => {
+  logger.error(err, 'burped error');
+  res.status(err.status || 500).json({msg: err.message});
+});
+app.listen(PORT);
+
+logger.info(`listening for HTTP requests on port ${PORT}`);
 
 module.exports = {srf};
