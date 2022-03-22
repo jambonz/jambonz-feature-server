567ff00c8cc7f93f111eff6741fef5dfc56848df
Dave Horton <daveh@beachdognet.com>
2022-03-21 20:36:10 -0400

    basic tracing for incoming calls

--- tracer.js
+++ tracer.js
@@ -7,48 +7,53 @@ const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 const { CollectorTraceExporter } =  require('@opentelemetry/exporter-collector');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
-const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
+//const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+//const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+//const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 
-
-const EXPORTER = process.env.EXPORTER || '';
 
 module.exports = (serviceName) => {
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-    }),
-  });
+  if (process.env.JAMBONZ_OTEL_ENABLED) {
+    const provider = new NodeTracerProvider({
+      resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+      }),
+    });
 
-  let exporter;
-  if (EXPORTER.toLowerCase().startsWith('z')) {
-    exporter = new ZipkinExporter();
-  } else {
-    exporter = new JaegerExporter();
+    let exporter;
+    if (process.env.OTEL_EXPORTER_JAEGER_AGENT_HOST) {
+      exporter = new JaegerExporter();
+    }
+    else if (process.env.OTEL_EXPORTER_ZIPKIN_URL) {
+      exporter = new ZipkinExporter({url:process.env.OTEL_EXPORTER_ZIPKIN_URL});
+    }
+    else {
+      exporter = new CollectorTraceExporter({
+        url: process.OTEL_EXPORTER_COLLECTOR_URL
+      });
+    }
+
+    provider.addSpanProcessor(new BatchSpanProcessor(exporter, {
+      // The maximum queue size. After the size is reached spans are dropped.
+      maxQueueSize: 100,
+      // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
+      maxExportBatchSize: 10,
+      // The interval between two consecutive exports
+      scheduledDelayMillis: 500,
+      // How long the export can run before it is cancelled
+      exportTimeoutMillis: 30000,
+    }));
+
+    // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
+    provider.register();
+    registerInstrumentations({
+      instrumentations: [
+        //new HttpInstrumentation(),
+        //new ExpressInstrumentation(),
+        //new PinoInstrumentation()
+      ],
+    });
   }
-
-  provider.addSpanProcessor(new BatchSpanProcessor(exporter, {
-    // The maximum queue size. After the size is reached spans are dropped.
-    maxQueueSize: 100,
-    // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
-    maxExportBatchSize: 10,
-    // The interval between two consecutive exports
-    scheduledDelayMillis: 500,
-    // How long the export can run before it is cancelled
-    exportTimeoutMillis: 30000,
-  }));
-
-  // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [
-      new HttpInstrumentation(),
-      new ExpressInstrumentation(),
-      new PinoInstrumentation()
-    ],
-  });
 
   return opentelemetry.trace.getTracer(serviceName);
 };
