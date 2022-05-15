9e14f88d077ba76343871910967fbaf3cc467fa2
Dave Horton <daveh@beachdognet.com>
2022-05-15 13:52:46 -0400

    updates to latest drachtio-srf and drachtio-fsmrf

--- test/docker_start.js
+++ test/docker_start.js
@@ -1,6 +1,5 @@
 const test = require('tape') ;
 const exec = require('child_process').exec ;
-const async = require('async');
 
 test('starting docker network..takes a bit for mysql and freeswitch to come up..patience..', (t) => {
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml up -d`, (err, stdout, stderr) => {
--- test/sip-tests.js
+++ test/sip-tests.js
@@ -1,4 +1,4 @@
-const test = require('blue-tape');
+const test = require('tape');
 const { output, sippUac } = require('./sipp')('test_sbc-inbound');
 const debug = require('debug')('drachtio:sbc-inbound');
 const clearModule = require('clear-module');
