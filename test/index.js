24ed13d3b23b3decfffdc0e3984ee1718eb3adb9
Quan Luu <quan.luuhoang8@gmail.com>
2022-08-09 12:37:04 +0700

    feat: add create-call timeout test

--- test/index.js
+++ test/index.js
@@ -6,5 +6,6 @@ require('./webhooks-tests');
 require('./say-tests');
 require('./gather-tests');
 require('./sip-request-tests');
+require('./create-call-test');
 require('./remove-test-db');
 require('./docker_stop');
