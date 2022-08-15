a6a50a4771e8fa90173643c7a3b9da1a62f93f31
Quan Luu <quan.luuhoang8@gmail.com>
2022-08-15 08:05:14 +0700

    feat: add alert for jambonz parsing falure

--- test/webhooks-tests.js
+++ test/webhooks-tests.js
@@ -1,6 +1,15 @@
 const test = require('tape');
 const { sippUac } = require('./sipp')('test_fs');
 const clearModule = require('clear-module');
+const provisionCallHook = require('./utils');
+const opts = {
+  timestamp: () => {return `, "time": "${new Date().toISOString()}"`;},
+  level: process.env.JAMBONES_LOGLEVEL || 'info'
+};
+const logger = require('pino')(opts);
+const { queryAlerts } = require('@jambonz/time-series')(
+  logger, process.env.JAMBONES_TIME_SERIES_HOST
+);
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
@@ -28,7 +37,7 @@ test('basic webhook tests', async(t) => {
         reason: 'Gone Fishin',
         headers: {
           'Retry-After': 300
-        }  
+        }
       }
     ];
 
@@ -45,3 +54,45 @@ test('basic webhook tests', async(t) => {
     t.error(err);
   }
 });
+
+test('invalid jambonz json create alert tests', async(t) => {
+  clearModule.all();
+  const {srf, disconnect} = require('../app');
+
+  try {
+    await connect(srf);
+
+    // GIVEN
+    // Invalid json array
+    const verbs = {
+      verb: 'say',
+      text: 'hello'
+    };
+
+    const from = 'invalid_json_create_alert';
+    provisionCallHook(from, verbs)
+
+    // THEN
+    await sippUac('uac-invite-expect-480.xml', '172.38.0.10', from);
+    // sleep testcase for more than 7 second to wait alert pushed to database.
+    await sleep(8000);
+    const data = await queryAlerts(
+      {account_sid: 'bb845d4b-83a9-4cde-a6e9-50f3743bab3f', page: 1, page_size: 25, days: 7});
+    // 'CallSid: eff9b0dd-253b-442e-92e8-906b47403bf2 error retrieving or parsing application: http://127.0.0.1:3100/,
+    // status: undefined, reason: malformed jambonz payload: must be array'
+    let checked = false;
+    for (let i = 0; i < data.total; i++) {
+      checked = data.data[i].message.includes('reason: malformed jambonz payload: must be array');
+    }
+    t.ok(checked, 'alert is raised as expected');
+    disconnect();
+  } catch (err) {
+    console.log(`error received: ${err}`);
+    disconnect();
+    t.error(err);
+  }
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
