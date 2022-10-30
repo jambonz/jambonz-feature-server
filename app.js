70caf00dd1b4f2c96b4cfb6f724bf66ff05a5a76
Dave Horton <daveh@beachdognet.com>
2022-10-30 13:07:49 -0400

    Feature/multi forks on ec2 (#182)
    * changes to allow multiple instances to run in an EC2 autoscale deployment
    
    * fix health check
    
    * fixup aws sns notification so it subscribes using bound port
    
    * AWS SNS port range 3010-3019

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const tracer = require('./tracer')(process.env.JAMBONES_OTEL_SERVICE_NAME || 'ja
 const api = require('@opentelemetry/api');
 srf.locals = {...srf.locals, otel: {tracer, api}};
 
-const PORT = process.env.HTTP_PORT || 3000;
 const opts = {level: process.env.JAMBONES_LOGLEVEL || 'info'};
 const pino = require('pino');
 const logger = pino(opts, pino.destination({sync: false}));
@@ -32,17 +31,6 @@ const {
   retrieveApplication,
   invokeWebCallback
 } = require('./lib/middleware')(srf, logger);
-
-// HTTP
-const express = require('express');
-const helmet = require('helmet');
-const app = express();
-Object.assign(app.locals, {
-  logger,
-  srf
-});
-
-const httpRoutes = require('./lib/http-routes');
 
 const InboundCallSession = require('./lib/session/inbound-call-session');
 const SipRecCallSession = require('./lib/session/siprec-call-session');
@@ -82,20 +70,6 @@ srf.invite(async(req, res) => {
   session.exec();
 });
 
-// HTTP
-app.use(helmet());
-app.use(helmet.hidePoweredBy());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/', httpRoutes);
-app.use((err, req, res, next) => {
-  logger.error(err, 'burped error');
-  res.status(err.status || 500).json({msg: err.message});
-});
-const httpServer = app.listen(PORT);
-
-logger.info(`listening for HTTP requests on port ${PORT}, serviceUrl is ${srf.locals.serviceUrl}`);
-
 const sessionTracker = srf.locals.sessionTracker = require('./lib/session/session-tracker');
 sessionTracker.on('idle', () => {
   if (srf.locals.lifecycleEmitter.operationalState === LifeCycleEvents.ScaleIn) {
@@ -103,19 +77,30 @@ sessionTracker.on('idle', () => {
     srf.locals.lifecycleEmitter.scaleIn();
   }
 });
-
 const getCount = () => sessionTracker.count;
 const healthCheck = require('@jambonz/http-health-check');
-healthCheck({app, logger, path: '/', fn: getCount});
+let httpServer;
+
+const createHttpListener = require('./lib/utils/http-listener');
+createHttpListener(logger, srf)
+  .then(({server, app}) => {
+    httpServer = server;
+    healthCheck({app, logger, path: '/', fn: getCount});
+    return {server, app};
+  })
+  .catch((err) => {
+    logger.error(err, 'Error creating http listener');
+  });
+
 
 setInterval(() => {
   srf.locals.stats.gauge('fs.sip.calls.count', sessionTracker.count);
-}, 5000);
+}, 20000);
 
 const disconnect = () => {
   return new Promise ((resolve) => {
-    httpServer.on('close', resolve);
-    httpServer.close();
+    httpServer?.on('close', resolve);
+    httpServer?.close();
     srf.disconnect();
     srf.locals.mediaservers.forEach((ms) => ms.disconnect());
   });
