8c0044a3787c2c633f2fc1a766f53c37f115a4e3
Hoan Luu Huu <110280845+xquanluu@users.noreply.github.com>
2023-01-24 21:04:37 +0700

    Weebhook app test support ws (#226)
    * feat: webhook test app support ws
    
    * feat: webhook test app support ws
    
    * feat: webhook test app support ws
    
    * feat: webhook test app support ws
    
    Co-authored-by: Quan HL <quanluuhoang8@gmail.com>

--- test/webhook/app.js
+++ test/webhook/app.js
@@ -1,13 +1,53 @@
-const assert = require('assert');
-const fs = require('fs');
 const express = require('express');
 const app = express();
+const Websocket = require('ws');
 const listenPort = process.env.HTTP_PORT || 3000;
 let json_mapping = new Map();
 let hook_mapping = new Map();
+let ws_packet_count = new Map();
+let ws_metadata = new Map();
 
-app.listen(listenPort, () => {
+/** websocket server for listen audio  */
+const recvAudio = (socket, req) => {
+  let packets = 0;
+  let path = req.url;
+  console.log('received websocket connection');
+  socket.on('message', (data, isBinary) => {
+    if (!isBinary) {
+      try {
+        const msg = JSON.parse(data);
+        console.log({msg}, 'received websocket message');
+        ws_metadata.set(path, msg);
+      }
+      catch (err) {
+        console.log({err}, 'error parsing websocket message');
+      }
+    }
+    else {
+      packets += data.length;
+    }
+  });
+  socket.on('error', (err) => {
+    console.log({err}, 'listen websocket: error');
+  });
+
+  socket.on('close', () => {
+    ws_packet_count.set(path, packets);
+  })
+};
+
+const wsServer = new Websocket.Server({ noServer: true });
+wsServer.setMaxListeners(0);
+wsServer.on('connection', recvAudio.bind(null));
+
+const server = app.listen(listenPort, () => {
   console.log(`sample jambones app server listening on ${listenPort}`);
+});
+server.on('upgrade', (request, socket, head) => {
+  console.log('received upgrade request');
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit('connection', socket, request);
+  });
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +60,7 @@ app.use(express.json());
 app.all('/', (req, res) => {
   console.log(req.body, 'POST /');
   const key = req.body.from
+  addRequestToMap(key, req, hook_mapping);
   return getJsonFromMap(key, req, res);
 });
 
@@ -91,6 +132,27 @@ app.get('/lastRequest/:key', (req, res) => {
   if (hook_mapping.has(key)) {
     let requests = hook_mapping.get(key);
     return res.json(requests[requests.length - 1]);
+  } else {
+    return res.sendStatus(404);
+  }
+})
+
+// WS Fetch
+app.get('/ws_packet_count/:key', (req, res) => {
+  let key = `/${req.params.key}`;
+  console.log(key, ws_packet_count);
+  if (ws_packet_count.has(key)) {
+    return res.json({ count: ws_packet_count.get(key) });
+  } else {
+    return res.sendStatus(404);
+  }
+})
+
+app.get('/ws_metadata/:key', (req, res) => {
+  let key = `/${req.params.key}`;
+  console.log(key, ws_packet_count);
+  if (ws_metadata.has(key)) {
+    return res.json({ metadata: ws_metadata.get(key) });
   } else {
     return res.sendStatus(404);
   }
