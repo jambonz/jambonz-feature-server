3414ee0e169fc828b41eb09738173e97842257f4
Quan HL <quanluuhoang8@gmail.com>
2023-01-27 08:56:32 +0700

    fix: uncomment testsuite

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 require('./unit-tests');
-// First Blog
 require('./docker_start');
 require('./create-test-db');
 require('./account-validation-tests');
+require('./dial-tests');
 require('./webhooks-tests');
 require('./say-tests');
 require('./gather-tests');
@@ -10,13 +10,6 @@ require('./transcribe-tests');
 require('./sip-request-tests');
 require('./create-call-test');
 require('./play-tests');
-require('./remove-test-db');
-require('./docker_stop');
-
-// 2nd blog
-require('./docker_start');
-require('./create-test-db');
-require('./dial-tests');
 require('./listen-tests');
 require('./remove-test-db');
 require('./docker_stop');
