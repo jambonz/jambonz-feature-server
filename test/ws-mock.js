f34d2952d4a06f85035729a48c6879b5ee1f3b1f
Quan HL <quanluuhoang8@gmail.com>
2023-02-05 00:48:05 +0700

    ws-requestor unit test

---
+++ test/ws-mock.js
@@ -0,0 +1,83 @@
+class MockWebsocket {
+    static eventResponses = new Map();
+    static actionLoops = new Map();
+    eventListeners = new Map();
+
+    constructor(url, protocols, options) {
+        this.u = url;
+        this.pros = protocols;
+        this.opts = options;
+        setTimeout(() => {
+            this.open();
+        }, 500)
+    }
+
+    static addJsonMapping(key, value) {
+        MockWebsocket.eventResponses.set(key, value);
+    }
+
+    static getAndIncreaseActionLoops(key) {
+        const ret = MockWebsocket.actionLoops.has(key) ? MockWebsocket.actionLoops.get(key) : 0;
+        MockWebsocket.actionLoops.set(key, ret + 1);
+        return ret;
+    }
+
+    once(event, listener) {
+        // Websocket.ws = this;
+        this.eventListeners.set(event, listener);
+        return this;
+    }
+
+    on(event, listener) {
+        // Websocket.ws = this;
+        this.eventListeners.set(event, listener);
+        return this;
+    }
+
+    open() {
+        if (this.eventListeners.has('open')) {
+            this.eventListeners.get('open')();
+        }
+    }
+
+    send(data, callback) {
+        const json = JSON.parse(data);
+        if (MockWebsocket.eventResponses.has(json.call_sid)) {
+
+            const resp_data = MockWebsocket.eventResponses.get(json.call_sid);
+            const action = resp_data.action[MockWebsocket.getAndIncreaseActionLoops(json.call_sid)];
+            if (action === 'connect') {
+                setTimeout(()=> {
+                    const msg = {
+                        type: 'ack',
+                        msgid: json.msgid,
+                        command: 'command',
+                        call_sid: json.call_sid,
+                        queueCommand: false, 
+                        data: resp_data.body}
+                    this.mockOnMessage(JSON.stringify(msg));
+                }, 100);
+            } else if (action === 'close') {
+                if (this.eventListeners.has('close')) {
+                    this.eventListeners.get('close')(500);
+                }
+            } else if (action === 'terminated') {
+                if (this.eventListeners.has('close')) {
+                    this.eventListeners.get('close')(1000);
+                }
+            }
+            
+        }
+        if (callback) {
+            callback();
+        }
+    }
+
+    mockOnMessage(message, isBinary=false) {
+        if (this.eventListeners.has('message')) {
+            this.eventListeners.get('message')(message, isBinary);
+        }
+    }
+}
+
+module.exports = MockWebsocket;
---
+++ test/ws-requestor-unit-test.js
@@ -0,0 +1,99 @@
+const test = require('tape');
+const sinon = require('sinon');
+const proxyquire = require("proxyquire");
+proxyquire.noCallThru();
+const MockWebsocket = require('./ws-mock')
+
+const logger = {
+  debug: (data) => {console.log(data)},
+  info: (data) => {console.log(data)}
+};
+
+const BaseRequestor = proxyquire(
+  "../lib/utils/base-requestor",
+  {
+    "../../": {
+      srf: {
+        locals: {
+          stats: {
+            histogram: () => {}
+          }
+        }
+      }
+    },
+    "@jambonz/time-series": sinon.stub()
+  }
+ );
+
+ const WsRequestor = proxyquire(
+  "../lib/utils/ws-requestor",
+  {
+    "./base-requestor": BaseRequestor,
+    "ws": MockWebsocket
+  }
+ );
+
+test('ws success', async (t) => {
+  // GIVEN
+
+  const json = '[{\"verb\": \"play\",\"url\": \"silence_stream://5000\"}]';
+  const ws_response = {
+    action: ['connect'],
+    body: json
+  }
+  const call_sid = 'ws_success';
+
+  MockWebsocket.addJsonMapping(call_sid, ws_response);
+
+  const hook = {
+    url: 'ws://localhost:3000',
+    username: 'username',
+    password: 'password'
+  }
+
+  const params = {
+    callSid: call_sid
+  }
+
+  // WHEN
+  
+  const requestor = new WsRequestor(logger, "account_sid", hook, "webhook_secret");
+  const result = await requestor.request('session:new',hook, params, {});
+
+  // THEN
+  t.ok(result == json,'ws successfully download jambonz json');
+
+  t.end();
+});
+
+test('ws close success reconnect', async (t) => {
+  // GIVEN
+
+  const call_sid = 'ws_closed'
+  const json = '[{\"verb\": \"play\",\"url\": \"silence_stream://5000\"}]';
+  const ws_response = {
+    action: ['close', 'connect'],
+    body: json
+  }
+  MockWebsocket.addJsonMapping(call_sid, ws_response);
+
+  const hook = {
+    url: 'ws://localhost:3000',
+    username: 'username',
+    password: 'password'
+  }
+
+  const params = {
+    callSid: call_sid
+  }
+
+  // WHEN
+  
+  const requestor = new WsRequestor(logger, "account_sid", hook, "webhook_secret");
+  const result = await requestor.request('session:new',hook, params, {});
+
+  // THEN
+  t.ok(result == json,'ws successfully download jambonz json');
+
+  t.end();
+});
