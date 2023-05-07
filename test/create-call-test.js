534268650dd5007465ab304dca35b697d5b10d96
Quan HL <quan.luuhoang8@gmail.com>
2023-05-07 21:14:26 +0700

    test amd

--- test/create-call-test.js
+++ test/create-call-test.js
@@ -19,56 +19,108 @@ function connect(connectable) {
   });
 }
 
-test('test create-call timeout', async(t) => {
+// test('test create-call timeout', async(t) => {
+//   clearModule.all();
+//   const {srf, disconnect} = require('../app');
+
+//   try {
+//     await connect(srf);
+
+//     // give UAS app time to come up
+//     const p = sippUac('uas-timeout-cancel.xml', '172.38.0.10');
+//     await waitFor(1000);
+
+//     // GIVEN
+//     let account_sid = '622f62e4-303a-49f2-bbe0-eb1e1714e37a';
+//     const post = bent('http://127.0.0.1:3000/', 'POST', 'json', 201);
+//     post('v1/createCall', {
+//       'account_sid':account_sid,
+//       'timeout': 1,
+//       "call_hook": {
+//         "url": "https://public-apps.jambonz.us/hello-world",
+//         "method": "POST"
+//       },
+//       "from": "15083718299",
+//       "to": {
+//         "type": "phone",
+//         "number": "15583084809"
+//       }});
+//     //THEN
+//     await p;
+//     disconnect();
+//   } catch (err) {
+//     console.log(`error received: ${err}`);
+//     disconnect();
+//     t.error(err);
+//   }
+// });
+
+// test('test create-call call-hook basic authentication', async(t) => {
+//   clearModule.all();
+//   const {srf, disconnect} = require('../app');
+
+//   try {
+//     await connect(srf);
+
+
+//     // GIVEN
+//     let from = 'call_hook_basic_authentication';
+//     let account_sid = '622f62e4-303a-49f2-bbe0-eb1e1714e37a';
+
+//     // Give UAS app time to come up
+//     const p = sippUac('uas.xml', '172.38.0.10', from);
+//     await waitFor(1000);
+
+//     const post = bent('http://127.0.0.1:3000/', 'POST', 'json', 201);
+//     post('v1/createCall', {
+//       'account_sid':account_sid,
+//       "call_hook": {
+//         "url": "http://127.0.0.1:3100/",
+//         "method": "POST",
+//         "username": "username",
+//         "password": "password"
+//       },
+//       "from": from,
+//       "to": {
+//         "type": "phone",
+//         "number": "15583084809"
+//       }});
+
+//     let verbs = [
+//       {
+//         "verb": "say",
+//         "text": "hello"
+//       }
+//     ];
+//     provisionCallHook(from, verbs);
+//     //THEN
+//     await p;
+
+//     let obj = await getJSON(`http:127.0.0.1:3100/lastRequest/${from}`)
+//     t.ok(obj.headers.Authorization = 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=',
+//       'create-call: call-hook contains basic authentication header');
+//     disconnect();
+//   } catch (err) {
+//     console.log(`error received: ${err}`);
+//     disconnect();
+//     t.error(err);
+//   }
+// });
+
+test('test create-call amd', async(t) => {
   clearModule.all();
   const {srf, disconnect} = require('../app');
 
   try {
     await connect(srf);
 
-    // give UAS app time to come up
-    const p = sippUac('uas-timeout-cancel.xml', '172.38.0.10');
-    await waitFor(1000);
 
     // GIVEN
-    let account_sid = '622f62e4-303a-49f2-bbe0-eb1e1714e37a';
-    const post = bent('http://127.0.0.1:3000/', 'POST', 'json', 201);
-    post('v1/createCall', {
-      'account_sid':account_sid,
-      'timeout': 1,
-      "call_hook": {
-        "url": "https://public-apps.jambonz.us/hello-world",
-        "method": "POST"
-      },
-      "from": "15083718299",
-      "to": {
-        "type": "phone",
-        "number": "15583084809"
-      }});
-    //THEN
-    await p;
-    disconnect();
-  } catch (err) {
-    console.log(`error received: ${err}`);
-    disconnect();
-    t.error(err);
-  }
-});
-
-test('test create-call call-hook basic authentication', async(t) => {
-  clearModule.all();
-  const {srf, disconnect} = require('../app');
-
-  try {
-    await connect(srf);
-
-
-    // GIVEN
-    let from = 'call_hook_basic_authentication';
-    let account_sid = '622f62e4-303a-49f2-bbe0-eb1e1714e37a';
+    let from = 'create-call-amd';
+    let account_sid = 'bb845d4b-83a9-4cde-a6e9-50f3743bab3f';
 
     // Give UAS app time to come up
-    const p = sippUac('uas.xml', '172.38.0.10', from);
+    const p = sippUac('uas-play.xml', '172.38.0.10', from);
     await waitFor(1000);
 
     const post = bent('http://127.0.0.1:3000/', 'POST', 'json', 201);
@@ -84,7 +136,12 @@ test('test create-call call-hook basic authentication', async(t) => {
       "to": {
         "type": "phone",
         "number": "15583084809"
-      }});
+      },
+      "amd": {
+        "actionHook": "/actionHook"
+      },
+      "speech_recognizer_vendor": "google"
+    });
 
     let verbs = [
       {
@@ -96,9 +153,8 @@ test('test create-call call-hook basic authentication', async(t) => {
     //THEN
     await p;
 
-    let obj = await getJSON(`http:127.0.0.1:3100/lastRequest/${from}`)
-    t.ok(obj.headers.Authorization = 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=',
-      'create-call: call-hook contains basic authentication header');
+    let obj = await getJSON(`http:127.0.0.1:3100/lastRequest/${from}_actionHook`)
+    console.log(obj);
     disconnect();
   } catch (err) {
     console.log(`error received: ${err}`);
