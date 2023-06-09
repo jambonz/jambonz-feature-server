aad24744f3b4444538c649c1927ceef2b3cebb28
Hoan Luu Huu <110280845+xquanluu@users.noreply.github.com>
2023-06-10 01:54:53 +0700

    feat: record all calls (#352)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix jslint
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix: add file ext
    
    * fix: record format
    
    * fix outbound
    
    * update to drachtio-fsmrf with support for multiple recording streams on a call
    
    * enable DTMF during background listen recording
    
    * fix merge commit
    
    ---------
    
    Co-authored-by: Dave Horton <daveh@beachdognet.com>

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const tracer = require('./tracer')(JAMBONES_OTEL_SERVICE_NAME);
 const api = require('@opentelemetry/api');
 srf.locals = {...srf.locals, otel: {tracer, api}};
 
-const opts = {level: JAMBONES_LOGLEVEL};
+const opts = {
+  level: JAMBONES_LOGLEVEL
+};
 const pino = require('pino');
 const logger = pino(opts, pino.destination({sync: false}));
 const {LifeCycleEvents, FS_UUID_SET_NAME} = require('./lib/utils/constants');
